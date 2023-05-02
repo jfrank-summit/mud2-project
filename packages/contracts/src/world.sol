239b803c0c9f92f518eb72a3d3604ed7b485efef
Jeremy Frank <jeremy@subspace.network>
2023-05-02 06:42:05 -0600

    add remix compiler config and world contract for remix

---
+++ packages/contracts/src/world.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.8.0;
+
+interface IModule {
+  error RequiredModuleNotFound(string resourceSelector);
+
+  /**
+   * Return the module name as a bytes16.
+   */
+  function getName() external view returns (bytes16 name);
+
+  /**
+   * A module expects to be called via the World contract, and therefore installs itself on its `msg.sender`.
+   */
+  function install(bytes memory args) external;
+}
+contract World {
+    function installRootModule(IModule module, bytes memory args) public {}
+}
